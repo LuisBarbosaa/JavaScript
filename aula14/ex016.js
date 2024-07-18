97c5cc20a2acd2d52b5eb9843c25e91045668126
LuisBarbosaa <luisbarbosaa1212@gmail.com>
2024-07-18 15:16:11 -0300

    create ex016

---
+++ aula14/ex016.js
@@ -0,0 +1,19 @@
+var cont = 1
+while(cont <= 10){
+    console.log(`Numero ${cont}`)
+    cont++
+}
+
+var cont = 1
+do{
+    cont++
+    console.log(`Numero ${cont}`)
+} while(cont <= 10)
+
+for (cont = 1; cont <= 10; cont++){
+    console.log(`Numero ${cont}`)
+}
+
+/*
+    Todos tem a mesma funcionalidade!
+*/
